89354357881d065a6ee7229d6fe4fdbd6fb9e12f
monsterooo <createthink.net@gmail.com>
2024-08-12 22:02:05 +0800

    feat: mdx render

--- src/app/(docs)/docs/layout.tsx
+++ src/app/(docs)/docs/layout.tsx
@@ -1,3 +1,6 @@
+import { docsConfig } from "@/config/docs"
+import DocsSidebarNav from "@/components/sidebar-nav"
+
 interface IProps {
   children: React.ReactNode
 }
@@ -6,7 +9,7 @@ export default function DocsLayout({ children }: IProps) {
   return (
     <div className="flex-1 md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10">
       <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r py-6 pr-2 md:sticky md:block lg:py-10">
-        sidebar
+        <DocsSidebarNav items={docsConfig.sidebarNav} />
       </aside>
       {children}
     </div>
